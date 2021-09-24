07cbbbd83ba9c4736f2cc3e8bc4a05d27e3813d9
Your Name <anishpandey021@gmail.com>
2021-09-24 18:59:54 +0545

    contact

--- script.js
+++ script.js
@@ -9,26 +9,32 @@ const About = document.getElementById("menu2");
 const Skill = document.getElementById("menu3");
 const Contact = document.getElementById("menu4");
 const menuBar = document.getElementById("navigation");
-const HTML = document.getElementById('html');
-const HTMLlogo = document.getElementById('htmllogo');
-const Python = document.getElementById('python');
-const Pythonlogo = document.getElementById('pythonlogo');
-const CSS = document.getElementById('css');
-const CSSlogo = document.getElementById('csslogo');
-const React = document.getElementById('react');
-const Reactlogo = document.getElementById('reactlogo');
-const JS = document.getElementById('js');
-const JSlogo = document.getElementById('jslogo');
-const Github = document.getElementById('github');
-const Githublogo = document.getElementById('githublogo');
-const defaultText = document.getElementById('default')
+const HTML = document.getElementById("html");
+const HTMLlogo = document.getElementById("htmllogo");
+const Python = document.getElementById("python");
+const Pythonlogo = document.getElementById("pythonlogo");
+const CSS = document.getElementById("css");
+const CSSlogo = document.getElementById("csslogo");
+const React = document.getElementById("react");
+const Reactlogo = document.getElementById("reactlogo");
+const JS = document.getElementById("js");
+const JSlogo = document.getElementById("jslogo");
+const Github = document.getElementById("github");
+const Githublogo = document.getElementById("githublogo");
+const defaultText = document.getElementById("default");
+let box = document.getElementById("infobox");
+let quitButton = document.getElementById("quit");
+
+let shown = true;
 
 function getOne() {
-    let selected = Math.floor(((Math.random() * noOfItems)));
+    let selected = Math.floor(Math.random() * noOfItems);
     document.querySelector("span#type").innerHTML = arr[selected];
-    document.documentElement.style.setProperty("--wordLength", arr[selected].length);
+    document.documentElement.style.setProperty(
+        "--wordLength",
+        arr[selected].length
+    );
 }
-
 
 function getWidth() {
     let spanbox = document.getElementById("type");
@@ -40,21 +46,15 @@ setInterval(getOne, 4000);
 setInterval(getWidth, 4000);
 
 function showCard() {
-    let box = document.getElementById('infobox');
-    box.style.left = "50px";
+    if (shown) {
+        box.style.opacity = "1";
+    }
 }
-
-function hideCard() {
-
-    let box = document.getElementById('infobox');
-    box.style.left = "-400px";
-}
-
-document.getElementById("logo").onmouseover = () => { showCard(); }
-document.getElementById("logo").addEventListener("mouseout", hideCard);
+quitButton.onclick = () => {
+    box.style.opacity = "0";
+};
 let lastscroll = 0;
-window.addEventListener('scroll', () => {
-
+window.addEventListener("scroll", () => {
     let currentscroll = Math.floor(window.scrollY);
     if (currentscroll >= 0 && currentscroll <= 400) {
         Home.style.color = "#00FFF0";
@@ -78,105 +78,106 @@ window.addEventListener('scroll', () => {
         Contact.style.color = "#fff";
     }
     if (currentscroll > lastscroll) {
-        menuBar.style.top = "-24px"
-
+        menuBar.style.top = "-24px";
     } else {
-        menuBar.style.top = "15px"
-
+        menuBar.style.top = "15px";
     }
     lastscroll = currentscroll;
     let scrollPercent = Math.floor((window.scrollY / 2261) * 100);
     let scrollPercentInv = 100 - scrollPercent;
-    document.documentElement.style.setProperty("--scrollPercent", scrollPercentInv + "%");
-})
+    document.documentElement.style.setProperty(
+        "--scrollPercent",
+        scrollPercentInv + "%"
+    );
+});
 
 function showContactCard() {
-    let email = prompt("enter your email", 'example@gmail.com');
+    let email = prompt("enter your email", "example@gmail.com");
     console.log(email);
 }
-defaultText.style.display = "block"
+defaultText.style.display = "block";
 HTML.style.display = "None";
 React.style.display = "None";
 Python.style.display = "None";
 CSS.style.display = "None";
 JS.style.display = "None";
 Github.style.display = "None";
-HTMLlogo.addEventListener('click', () => {
-    console.log("clicked")
-    document.documentElement.style.setProperty("--AnimationForHTML", "HTML 1s ease-in-out forwards");
+HTMLlogo.addEventListener("click", () => {
+    console.log("clicked");
+    document.documentElement.style.setProperty(
+        "--AnimationForHTML",
+        "HTML 1s ease-in-out forwards"
+    );
     React.style.display = "None";
     Python.style.display = "None";
     CSS.style.display = "none";
     JS.style.display = "None";
     Github.style.display = "None";
     HTML.style.display = "Block";
-    defaultText.style.display = "none"
-
-
-
-})
-CSSlogo.addEventListener('click', () => {
-    document.documentElement.style.setProperty("--AnimationForCSS", "CSS 1s ease-in-out forwards")
+    defaultText.style.display = "none";
+});
+CSSlogo.addEventListener("click", () => {
+    document.documentElement.style.setProperty(
+        "--AnimationForCSS",
+        "CSS 1s ease-in-out forwards"
+    );
     React.style.display = "None";
     Python.style.display = "none";
     CSS.style.display = "block";
     JS.style.display = "None";
     Github.style.display = "None";
     HTML.style.display = "none";
-    defaultText.style.display = "none"
-
-
-
+    defaultText.style.display = "none";
 });
-JSlogo.addEventListener('click', () => {
-    document.documentElement.style.setProperty("--AnimationForJS", "JS 1s ease-in-out forwards")
+JSlogo.addEventListener("click", () => {
+    document.documentElement.style.setProperty(
+        "--AnimationForJS",
+        "JS 1s ease-in-out forwards"
+    );
     React.style.display = "None";
     Python.style.display = "none";
     CSS.style.display = "none";
     JS.style.display = "block";
     Github.style.display = "None";
     HTML.style.display = "none";
-    defaultText.style.display = "none"
-
-
-
+    defaultText.style.display = "none";
 });
-Pythonlogo.addEventListener('click', () => {
-    document.documentElement.style.setProperty("--AnimationForPYTHON", "PYTHON 1s ease-in-out forwards")
+Pythonlogo.addEventListener("click", () => {
+    document.documentElement.style.setProperty(
+        "--AnimationForPYTHON",
+        "PYTHON 1s ease-in-out forwards"
+    );
     React.style.display = "None";
     Python.style.display = "BLOCK";
     CSS.style.display = "none";
     JS.style.display = "none";
     Github.style.display = "None";
     HTML.style.display = "none";
-    defaultText.style.display = "none"
-
-
-
+    defaultText.style.display = "none";
 });
-Reactlogo.addEventListener('click', () => {
-    document.documentElement.style.setProperty("--AnimationForReact", "REACT 1s ease-in-out forwards")
+Reactlogo.addEventListener("click", () => {
+    document.documentElement.style.setProperty(
+        "--AnimationForReact",
+        "REACT 1s ease-in-out forwards"
+    );
     React.style.display = "block";
     Python.style.display = "none    ";
     CSS.style.display = "none";
     JS.style.display = "none";
     Github.style.display = "None";
     HTML.style.display = "none";
-    defaultText.style.display = "none"
-
-
-
+    defaultText.style.display = "none";
 });
-Githublogo.addEventListener('click', () => {
-    document.documentElement.style.setProperty("--AnimationForGithub", "GITHUB 1s ease-in-out forwards")
+Githublogo.addEventListener("click", () => {
+    document.documentElement.style.setProperty(
+        "--AnimationForGithub",
+        "GITHUB 1s ease-in-out forwards"
+    );
     React.style.display = "none";
     Python.style.display = "none    ";
     CSS.style.display = "none";
     JS.style.display = "none";
     Github.style.display = "block";
     HTML.style.display = "none";
-    defaultText.style.display = "none"
-
-
-
+    defaultText.style.display = "none";
 });
